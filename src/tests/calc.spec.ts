f5cf70b7f2d6701e15cb6395b7bd47a39f81e0c5
Leandro Gehlen <legedeveloper@gmail.com>
2023-05-04 10:28:39 -0300

    es lint seems to work. Trying with husky before commit right now.

--- src/tests/calc.spec.ts
+++ src/tests/calc.spec.ts
@@ -1,10 +1,10 @@
-import { Add } from "@/calc";
+import { Add } from '@/calc';
 
-describe("test add function", () => {
-  it("should return 15 for add(10,5)", () => {
-    expect(Add(10, 3)).toBe(13);
+describe('test add function', () => {
+  it('should return 15 for add(10,5)', () => {
+    expect(Add(10, 5)).toBe(15);
   });
-it("should return 5 for add(2,3)", () => {
+  it('should return 5 for add(2,3)', () => {
     expect(Add(2, 3)).toBe(5);
   });
 });
