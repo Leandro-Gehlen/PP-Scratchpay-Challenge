3fcc6122eebbdb2951e75672cc6ac011e372c0c8
Leandro Gehlen <legedeveloper@gmail.com>
2023-05-05 01:10:26 -0300

    sut.dentalHelper returns an HttpResponseType

--- src/calc.ts
+++
@@ -1,3 +0,0 @@
-export const Add = (x:number, y:number): number=>{
-    return x+y
-}
--- src/domain-layer/usecases/detal-helper.spec.ts
+++ src/domain-layer/usecases/detal-helper.spec.ts
@@ -1,14 +1,26 @@
 import { DentalEntity, EnumEntityCategory } from "@/domain-layer/entities";
 
 export interface IDentalHelperService {
-  dentalHelper(query: DentalEntity): Promise<any>;
+  dentalHelper(query: DentalEntity): Promise<HttpResponseType>;
 }
 
 export class DentalHelperService implements IDentalHelperService {
-  async dentalHelper(query: DentalEntity): Promise<any> {
-    return;
+  async dentalHelper(query: DentalEntity): Promise<HttpResponseType> {
+    return {
+      statusCode: 200,
+      data: [
+        {
+          category: EnumEntityCategory.DENTAL,
+        },
+      ],
+    };
   }
 }
+
+export type HttpResponseType = {
+  statusCode: number;
+  data: Array<DentalEntity>;
+};
 // ---------------------------- Production Code ----------------------------
 const sutFactory = () => {
   const sut = new DentalHelperService();
@@ -22,6 +34,17 @@ describe("DentalHelperService class as SUT", () => {
       category: EnumEntityCategory.DENTAL,
     };
     const hasMethod = await sut.dentalHelper(arg);
-    expect(hasMethod).toBe(await sut.dentalHelper(arg));
+    expect(hasMethod).toStrictEqual(await sut.dentalHelper(arg));
+  });
+  it("Should return an HttpResponseContract", async () => {
+    const sut = sutFactory();
+    const arg: DentalEntity = {
+      category: EnumEntityCategory.DENTAL,
+    };
+    const HttpResponse: HttpResponseType = {
+      statusCode: 200,
+      data: [arg],
+    };
+    expect(await sut.dentalHelper(arg)).toStrictEqual(HttpResponse);
   });
 });
