6fbbb27366cc46785b30c8c1ddfc24b824079d8e
Leandro Gehlen <legedeveloper@gmail.com>
2023-05-04 23:11:34 -0300

    entities created on domain folder

---
+++ src/domain-layer/entities/entities.ts
@@ -0,0 +1,21 @@
+export type HealthEntity = {
+  name: string;
+  stateName: string;
+  availability: AvailabilityType;
+};
+
+export type AvailabilityType = {
+  from: string;
+  to: string;
+};
+
+export type VetEntity = {
+  clinicName: string;
+  stateCode: string;
+  opening: OpeningType;
+};
+
+export type OpeningType = {
+  from: string;
+  to: string;
+};
